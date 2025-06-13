7955a02320c467cde693712d874cbef74dec093d
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2025-06-14 01:51:19 +0900

    Create BOJ_N과 M (1).java

---
+++ 10주차/huiyeong/BOJ_N과 M (1).java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        visited = new boolean[N];
+
+        DFS(0);
+    }
+
+    private static void DFS(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i + 1;
+                DFS(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
