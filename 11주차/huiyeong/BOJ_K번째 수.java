631b21da9a00247db848921afa9c493f064d4d1e
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2025-06-16 22:48:19 +0900

    Create BOJ_K번째 수.java

---
+++ 11주차/huiyeong/BOJ_K번째 수.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+
+        int left = 1;
+        int right = K;
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            long count = 0;
+            for (int i = 1; i <= N; i++) {
+                count += Math.min(mid / i, N);
+            }
+
+            if (count < K) {
+                left = mid + 1;
+            } else {
+                answer = mid;
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
